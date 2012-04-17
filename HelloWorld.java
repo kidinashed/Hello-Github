0dbe7e8a25e58cb81cad5817a77ad7827f1ac78f
Roger Price <rnprice@gmx.com>
2012-04-17 11:42:43 +0100

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World".
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
